06a6db66c74b00ae8c7337d604f3f3d4ffe339a0
wasabi315 <wasabi.nvim.st@gmail.com>
2018-12-28 23:57:40 +0900

    implement test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,46 @@
+import           Data.Int
+import           Data.PieceTable
+import           Test.QuickCheck
+
+import qualified Data.Text.Lazy     as TL
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    quickCheck . verbose $
+        forAll genIx $ \i ->
+        forAll genText $ \t ->
+        forAll genText $ \s ->
+            propInsert i t s
+    quickCheck . verbose $
+        forAll genIx $ \i ->
+        forAll genIx $ \j ->
+        forAll genText $ \t ->
+            propDelete i j t
+
+genIx :: Gen Int64
+genIx = arbitrary
+
+genText :: Gen TL.Text
+genText = fmap TL.pack arbitrary
+
+ins :: Int64 -> TL.Text -> TL.Text -> TL.Text
+ins i s t
+    | i < 0 = t
+    | i > TL.length t = t
+    | otherwise = let (l, r) = TL.splitAt i t in l <> s <> r
+
+propInsert :: Int64 -> TL.Text -> TL.Text -> Bool
+propInsert i t pt
+    =  toText (insert i t (fromText pt))
+    == ins i t pt
+
+propDelete :: Int64 -> Int64 -> TL.Text -> Bool
+propDelete i j pt
+    =  toText (delete i j (fromText pt))
+    == del i j pt
+  where
+    del n m t = case compare n m of
+        LT -> TL.take n pt <> TL.drop m t
+        EQ -> t
+        GT -> del m n t
+
